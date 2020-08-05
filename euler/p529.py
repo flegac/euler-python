4af24e211fabf743532da4ab50f8266f01f91949
flegac <florent.legac@gmail.com>
2020-08-05 03:28:32 +0200

    p529: 10-substrings (just ideas

---
+++ euler/p529.py
@@ -0,0 +1,20 @@
+def p529(n: int):
+    '''
+    1) lister les partitions de l'entier 10:
+    A = {1: 10}, {2:1, 1:8}, {3:1, 1:7}, ...
+
+    2) Lister les mots formés par tous arangements des partitions ci-dessus
+    B = 3331, 154, 451, 55, 73, 37, ...
+
+    3) Les nombres recherchés sont composés de concaténations de ces chaines !!
+
+    4) Dénombrer les nombres 10-substring-friendly de longueur < 10**n
+    5) retourner le résultat modulo 1_000_000_007
+
+    '''
+
+    pass
+
+
+if __name__ == '__main__':
+    p529(5)
--- tests/test_p67.py
+++ tests/test_p67.py
@@ -1,5 +1,12 @@
 from euler.p67 import p67
+from euler.p529 import p529
 
 
 def test_p67():
     assert p67() == 7273
+
+
+def test_p529():
+    assert p529(2) == 9
+    assert p529(5) == 3492
+    assert p529(10 ** 18) == -1
