feat: fixing some overflow ... more to do

euler/lib/automate.py
@@ -1,11 +1,11 @@
 import json
 from collections import defaultdict
-from typing import Dict, List, Set, Tuple, Generator, Any, Callable
+from typing import Dict, Set, Tuple, Generator, Any, Callable
 
 import numpy as np
-from scipy.sparse import lil_matrix
 
 from euler.lib.timer import timer
+from euler.lib.utils import error, warning
 
 Edge = Tuple[str, str, str, int]
 
@@ -72,21 +72,13 @@ class Automate(object):
     def compute_terminals(self, is_terminal: Callable[[str], bool]):
         self.T = set(filter(is_terminal, self.S))
 
-    def error(self, test: bool, msg: str):
-        if not test:
-            raise ValueError(msg)
-
-    def warning(self, test: bool, msg: str):
-        if not test:
-            print('warning: {}'.format(msg))
-
     def validate(self):
         missing_transitions = []
         probabilistic_transitions = []
-        self.error(self.I in self.S, 'Initial state must be in S : {}'.format(self.I))
+        error(self.I in self.S, 'Initial state must be in S : {}'.format(self.I))
 
         for x in self.T:
-            self.error(x in self.S, 'Terminal states must be in S : {}'.format(x))
+            error(x in self.S, 'Terminal states must be in S : {}'.format(x))
         for s1, letter_edges in self.Q.items():
             if set(letter_edges.keys()) != self.A:
                 if len(missing_transitions) < 5:
@@ -94,7 +86,7 @@ class Automate(object):
                 elif len(missing_transitions) == 5:
                     missing_transitions.append('...')
             for a, edges in letter_edges.items():
-                self.error(a in self.A, 'Transition must be in A: {}'.format((s1, a)))
+                error(a in self.A, 'Transition must be in A: {}'.format((s1, a)))
                 if len(edges) > 1:
                     if len(probabilistic_transitions) < 5:
                         probabilistic_transitions.append((s1, a))
@@ -102,13 +94,13 @@ class Automate(object):
                         missing_transitions.append('...')
 
                 for s2, n in edges.items():
-                    self.error(s2 in self.S, 's2 states is not in S : (s1:{}, a:{}, s2:{}, n:{})'.format(s1, a, s2, n))
-                    self.error(n > 0, 'Transition must be in A')
+                    error(s2 in self.S, 's2 states is not in S : (s1:{}, a:{}, s2:{}, n:{})'.format(s1, a, s2, n))
+                    error(n > 0, 'Transition must be in A')
 
-        self.warning(len(missing_transitions) == 0,
-                     'Missing transitions: {}'.format(missing_transitions))
-        self.warning(len(probabilistic_transitions) == 0,
-                     'Automat is probabilistic: {}'.format(probabilistic_transitions))
+        warning(len(missing_transitions) == 0,
+                'Missing transitions: {}'.format(missing_transitions))
+        warning(len(probabilistic_transitions) == 0,
+                'Automat is probabilistic: {}'.format(probabilistic_transitions))
 
         self.index = {
             v: i
@@ -160,38 +152,16 @@ class Automate(object):
             for edge in self.transitions(s1):
                 yield edge
 
+    @timer
+    def to_matrix(self) -> np.ndarray:
+        n = len(self.S)
+        mat = np.zeros((n, n), dtype=np.uint64)
 
-@timer
-def update_classes(automate: Automate, classes: List[List[int]]):
-    # init mapping function :
-    mapping = dict()
-    cc = set()
-    for c in classes:
-        cc.add(c[0])
-        for x in c[1:]:
-            mapping[x] = c[0]
-
-    def f(s):
-        return mapping.get(s, s)
-
-    # check all
-    for s1, a, s2, n in automate.edges():
-        items = automate.find(f(s1), a)
-
-        if f(s2) not in set(map(f, items)):
-            print('-----------------------------------------------------------')
-            print('error on:', f(s1), a, f(s2), n)
-            print('found   :', items)
-            raise ValueError
-
-    # update edges (s1,a,s2,n) --> (s1,a,f(s2),n)
-    Q = Automate.from_scratch()
-    for s1, a, s2, n in automate.edges():
-        if not Q.find(f(s1), a):
-            Q.add(f(s1), a, f(s2), n)
-    Q.I = f(automate.I)
-    Q.T = set(map(f, automate.T))
-    return Q
+        for v1, _, v2, n in self.edges():
+            i = self.index[v1]
+            j = self.index[v2]
+            mat[i, j] += n
+        return mat
 
 
 def analyze_graph(automate: Automate):
@@ -213,75 +183,18 @@ def analyze_graph(automate: Automate):
 
 
 @timer
-def show_graph(graph: Automate, N: int = 100):
+def show_automate(aut: Automate, N: int = 500):
     from graphviz import Digraph
     dot = Digraph(comment='p529')
 
-    for x in graph.states[:N]:
-        dot.node(str(x), str(x))
-    for x in graph.states[:N]:
-        for a, y in graph.graph[x]:
-            if y in graph.states[:N]:
-                dot.edge(str(x), str(y))
+    S = set(list(sorted(aut.S))[:N])
+
+    for x in S:
+        dot.node(x, x)
+
+    for s1, a, s2, n in aut.edges():
+        if s1 == s2:
+            continue
+        if s1 in S and s2 in S:
+            dot.edge(s1, s2)
     dot.render('test-output/p259_{}.gv'.format(N), view=True)
-
-
-@timer
-def sparse_matrix(automate: Automate) -> np.ndarray:
-    n = len(automate.S)
-    mat = lil_matrix((n, n), dtype=np.uint)
-
-    for v1, _, v2, n in automate.edges():
-        i = automate.index[v1]
-        j = automate.index[v2]
-        mat[i, j] += n
-    return mat.tocsr()
-
-
-def minimize(aut: Automate):
-    F = frozenset(aut.T)
-    Q = frozenset(aut.S)
-    Q_F = frozenset(Q.difference(F))
-    P = {F, Q_F}
-    W = {
-        (min([F, Q_F], key=len), a)
-        for a in aut.A
-    }
-
-    while len(W) > 0:
-        Z, a = W.pop()
-        assert sum(map(len, P)) == len(aut.S)
-        P2 = set()
-        for X in P:
-            res = split(aut, Z, a, X)
-            for _ in res:
-                P2.add(_)
-            if len(res) == 2:
-                for b in aut.A:
-                    if (X, b) in W:
-                        W.remove((X, b))
-                        for _ in res:
-                            W.add((_, b))
-                    else:
-                        W.add((min(res, key=len), b))
-        P = P2
-    return None
-
-
-def split(aut: Automate, Z: Set[str], a: str, X: Set[str]):
-    A = set()
-    B = set()
-    for s1 in X:
-        img = aut.find(s1, a)
-        if len(img) == 0:
-            B.add(s1)
-        else:
-            for s2 in img:
-                if s2 in Z:
-                    A.add(s1)
-                else:
-                    B.add(s1)
-    assert len(A) + len(B) == len(X)
-    if len(A) == 0 or len(B) == 0:
-        return [X]
-    return [frozenset(A), frozenset(B)]

euler/lib/fast_power.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from typing import Callable, Any
 
 import numpy as np
-from scipy.sparse import csr_matrix
 
 
 class FastPower(object):
@@ -26,7 +25,7 @@ class FastPower(object):
         if self.path:
             path = self.full_path(n)
             if path.exists():
-                res = csr_matrix(np.load(path, allow_pickle=True))
+                res = np.load(path, allow_pickle=True).astype(np.uint64)
                 self.cache[n] = res
                 return res
 
@@ -37,6 +36,6 @@ class FastPower(object):
         self.cache[n] = res
 
         if self.path:
-            res.todense().dump(self.full_path(n))
+            res.astype(np.uint64).dump(self.full_path(n))
 
         return res

euler/prob_529/p529_solver.py
@@ -2,9 +2,11 @@ import json
 from collections import defaultdict
 from typing import Mapping
 
+import numpy as np
 from scipy.sparse import csr_matrix
 
-from euler.lib.automate import Automate, sparse_matrix, update_classes, minimize
+from euler.lib.automate import Automate
+from euler.lib.automate_minimize import update_classes
 from euler.lib.fast_power import FastPower
 from euler.lib.timer import timer
 from euler.prob_529.p529 import P529
@@ -22,6 +24,8 @@ class P529Solver(object):
         print('solver initialized :')
         self.automate.show_stats()
 
+        # partition = minimize(self.automate)
+
         with open('../resources/p529/partitions.json') as _:
             parts = json.load(_)
         print('partition:', len(parts))
@@ -31,24 +35,9 @@ class P529Solver(object):
         self.automate.save('automat_small.json')
         self.automate.show_stats()
 
-        xx = minimize(self.automate)
+        self.matrix = self.automate.to_matrix()
 
-        @timer
-        def matrix_mult(a, b):
-            x = a.dot(b)
-            if use_mod:
-                x = x.todense()
-                x %= MOD
-                x = csr_matrix(x)
-            try:
-                print('matrix.size:', x.nnz)
-            except:
-                pass
-
-            return x
-
-        self.matrix = sparse_matrix(self.automate)
-        self.mat_power = FastPower(self.matrix, matrix_mult, path='../resources/p529/mat')
+        # show_automate(self.automate, N=100)
 
     @property
     def terminals(self):
@@ -73,23 +62,39 @@ class P529Solver(object):
 
     @timer
     def word_number(self, nodes: Mapping):
-        tot = 0
+        tot = np.array([0]).astype(np.uint64)
         for v in self.terminals:
             tot += nodes.get(v, 0)
             if self.use_mod:
                 tot %= MOD
-        return tot
+        return tot[0]
 
     @timer
-    def compute_value(self, n: int):
+    def compute_value(self, n: int, cache_path: str = '../resources/p529/mat'):
+        if self.use_mod:
+            cache_path += 'mod'
+
         if n == 0:
             return 0
 
-        mat2 = self.mat_power.power(n)
-        tot = 0
+        @timer
+        def matrix_mult(a, b):
+            if self.use_mod:
+                a %= MOD
+                b %= MOD
+            a = csr_matrix(a).astype(np.uint64)
+            b = csr_matrix(b).astype(np.uint64)
+            x = a.dot(b).todense().astype(np.uint64)
+            if self.use_mod:
+                x %= MOD
+            return x
+
+        mat2 = FastPower(self.matrix, matrix_mult, path=cache_path).power(n)
+
+        tot = np.array([0]).astype(np.uint64)
         for v1 in P529Solver.SOURCE_NODES:
             for v2 in self.terminals:
                 tot += mat2[self.automate.index[v1], self.automate.index[v2]]
                 if self.use_mod:
                     tot %= MOD
-        return tot
+        return tot[0]